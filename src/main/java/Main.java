aff753dbdf29c513182ae424a0ebda9270cdf93a
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-03 05:22:52 +0000

    se agregó switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,8 +28,47 @@ public class Main {
     } else {
       System.out.println("El mes es: " + mes);
     }
-/* HACER EL SWITCH */
 
+    switch (mes) {
+        case 1:
+            System.out.println("Enero");
+            break;
+        case 2:
+            System.out.println("Febrero");
+            break;
+        case 3:
+            System.out.println("Marzo");
+            break;
+        case 4:
+            System.out.println("Abril");
+            break;
+        case 5:
+            System.out.println("Mayo");
+            break;
+        case 6:
+            System.out.println("Junio");
+            break;
+        case 7:
+            System.out.println("Julio");
+            break;
+        case 8:
+            System.out.println("Agosto");
+            break;
+        case 9:
+            System.out.println("Septiembre");
+            break;
+        case 10:
+            System.out.println("Octubre");
+            break;
+        case 11:
+            System.out.println("Noviembre");
+            break;
+        case 12:
+            System.out.println("Diciembre");
+            break;
+        default:
+            System.out.println("Número de mes inválido");
+    }
     input.close();
   }
 }
